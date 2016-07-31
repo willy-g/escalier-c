2fc8bcca3db007d8e00bcd995f6b6fbd8058de9f
willy-g <williamgalopin@gmail.com>
2016-07-31 14:28:16 +0900

    Add files via upload

---
+++ contact-form/contactthanks.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML">
+
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="../images/favicon.png">
+	
+	<title>escalier.C お問い合わせ</title>
+	
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="row text-center">
+			<br/>
+			<img class="img-circle" src="../images/escalier-carousel.jpg" alt="escalier-C" width="200" height="200"/>
+			<h2>お問い合わせフォーム</h2>
+			<p>By <a href="../index.html">escalier-C</a></p>			
+			
+		<br /><br />
+					
+			<h1>お問い合わせありがとうございました。</h1><br />
+		
+			<p><a href="../index.html">Back to Home</a></p>
+		</div>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
